775e9536d2d749c5aa4883d4897bd8e6b72c7b15
lirummirul <89711820+lirummirul@users.noreply.github.com>
2023-05-16 20:28:14 +0300

    removed unnecessary comments #2

--- poli.py
+++ poli.py
@@ -5,25 +5,17 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
 
-# Загрузка csv файла
 data = pd.read_csv('dataset.csv')
-
-# Определение зависимой переменной (атрибута, который нужно предсказать)
 target_variable = 'pts'
-
-# Определение независимых переменных (атрибутов, от которых зависит целевой атрибут)
 independent_variables = ['age', 'player_height', 'player_weight']
 
-# Разделение данных на обучающую и тестовую выборки
 X = data[independent_variables]
 y = data[target_variable]
-# Разделение данных на обучающий набор и тестовый набор
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # =================== PART 2 ======================
 
-# Преобразование независимых переменных в полиномиальные признаки
-degree = 2  # Степень полинома
+degree = 2
 poly_features = PolynomialFeatures(degree=degree)
 X_poly = poly_features.fit_transform(X)
 X_test_poly = poly_features.transform(X_test)
@@ -31,13 +23,9 @@ X_test_poly = poly_features.transform(X_test)
 model = LinearRegression()
 model.fit(X_poly, y)
 
-# Получаем предсказания модели для тестовых данных
 y_pred = model.predict(X_test_poly)
 
-# Вычисляем R^2 score для модели
 score = r2_score(y_test, y_pred)
-
-# Выводим значение score
 print("R² Score:", score)
 
 plt.scatter(X['player_height'], y, label='Исходные данные')
